86a432641e1dd06d26f962cab119c4cc6caf5741
LAB53 <lab53@fatec.com>
2025-05-05 09:12:25 -0300

    Finaliza Programa

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿double numero;
+Console.Write("Digite um número: ");
+numero = Convert.ToDouble(Console.ReadLine()!);
+
+if(numero % 2 == 0) {
+Console.WriteLine("Seu número é par");
+}
+
+else {
+Console.WriteLine("Seu número é ímpar:");
+}
